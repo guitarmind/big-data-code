25a02465890702dad00fcb2d0945a3533473aebe
guitarmind <markpeng73@gmail.com>
2015-11-13 15:06:07 +0800

    Revised Eclipse project issues.

--- genthrift.sh
+++ genthrift.sh
@@ -2,3 +2,8 @@ rm -rf gen-javabean src/java/manning/schema
 thrift -r --gen java:beans,hashcode,nocamel src/schema.thrift
 mv gen-javabean/manning/schema src/java/manning/
 rm -rf gen-javabean
+
+# markpeng
+mv src/java/manning src/
+rm -rf src/java
+
